2bc7d67e8e103655bb40376da41e06baff25d437
Stefania Iuga <sallystefy@gmail.com>
2022-12-01 19:45:53 +0200

    Update Mar.cpp

--- src/Mar.cpp
+++ src/Mar.cpp
@@ -20,12 +20,26 @@
         std::cout<<"Mar copy-constructor\n";
     }
 
+                                                    //Item 10 
+                                                    //Item 11
+                                                    //Item 12                                                    
     Mar& Mar::operator=(const Mar &m)
     {
+        if(this==&m)
+        {
+            std::cout<<"Mar assignment to self\n";
+            return *this;
+        }
+
         Fruct::operator=(m);
         std::cout<<"Mar assignment\n";
         this->type=m.type;
         return *this;
+    }
+
+    int Mar::getType()
+    {
+       return type;
     }
 
 
