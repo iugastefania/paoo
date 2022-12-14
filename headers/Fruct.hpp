234634c3cb4462196ba5d52b84f9d592fd8f0495
Stefania Iuga <sallystefy@gmail.com>
2022-12-14 23:51:33 +0200

    Update Fruct for Item 14

--- headers/Fruct.hpp
+++ headers/Fruct.hpp
@@ -6,8 +6,9 @@ class Fruct
     private:
         int greutate;
         double pret;
-                        //constructor initialize everything in the object (member initialization list instead of assignments)
-                        //order is correct
+        bool rotten;                // am adaugat un boolean pentru a verifica daca este rotten sau unrotten (Item 14)
+                                    // constructor initialize everything in the object (member initialization list instead of assignments)
+                                    // order is correct
 
     public:
         Fruct(int g, double p);   
@@ -19,6 +20,12 @@ class Fruct
         virtual ~Fruct();
 
         int getgreutate();
+
+                                        // Item 14 
+
+        void setIsRotten(bool r);       // functie pentru setare rotten sau unrotten
+
+        void verifyRotten();            // functie pentru verificare rotten sau unrotten
 };
 
 #endif 
